78cd1d3b85c1b987ddd539c1e8d68b9144387933
Ahmed-Alnasser <s202347950@kfupm.edu.sa>
2026-03-30 08:44:10 +0300

    starting the server on port 3000

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,12 +1,13 @@
 // TODO 1: Import Express
 import express from 'express';
+import cors from 'cors';
 
 
 // TODO 2: Create the Express app and store it in a variable named app
 const app = express();
 
 // TODO 3: Allow React to access the server
-
+app.use(cors());
 
 
 // TODO 5: Create the home route "/"
@@ -22,3 +23,6 @@ const app = express();
 
 
 // TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
